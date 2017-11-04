8f98b5a74640c871dae3ed178e76bee286c515cd
Jason Kulasingam <jason@jasons-mbp.attlocal.net>
2017-11-04 07:07:18 -0400

    LIRI Upload

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'ZMvVDUtTwcfKyxsyGyWuGhJbS',
+  consumer_secret: 'SJ9mzT8atwiKnicWjO5D4wLYUwawHDH3xvgoM9oU9WwiYM6ovc',
+  access_token_key: '22964735-zj7rWFVQ2sREoAHq19gp2Mhj3xflWK8GlAYxRuyMj',
+  access_token_secret: 'V27o1N5PZe8FQKr5hUBKZeiY9zju0ktdxKozHW2XQq6AF',
+}
+
+module.exports = twitterKeys;
+
+//https://apps.twitter.com/app/14431693
+//spotify client ID 150054f6f2b54d37b20f6db82873a412
+//spotify client secret ba17ff6d1bfe4ce684c265138acb8a51
